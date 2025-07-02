b4eb4eabe4636186766f4dabb1da0623b6c0420b
Jeducious <jamesharrycrowther@gmail.com>
2025-07-02 13:17:57 +1000

    Add automated tests

---
+++ tests/__init__.py
@@ -0,0 +1,2 @@
+# Copyright 2025 James Crowther
+# SPDX-License-Identifier: GPL-3.0-only
---
+++ tests/round_trip_tests.py
@@ -0,0 +1,27 @@
+# Copyright 2025 James Crowther
+# SPDX-License-Identifier: GPL-3.0-only
+
+from blend_requests.bl_ops_classes import FINISHED, BR_OT_event_bus_base
+TEST_TOPIC = b"test.topic"
+
+class CR_OT_event_bus_test_2(BR_OT_event_bus_base):
+
+    bl_idname = "crowdrender.event_bus_test_2"
+    bl_label = "test op 2"
+    subscription_topic = TEST_TOPIC
+
+    @classmethod
+    def poll(cls, bl_ctx: bpy.types.Context):
+        return True
+
+    def request(self):
+
+        # generate the request event.
+        self.publish_event(HOST_APP, self.subscription_topic, b"Hi!")
+
+    def handle_response(self) -> Set[str]:
+
+        bevent = BusEvent.deserialise(self.sub.recv_multipart())
+        self.report({"INFO"}, f"got {bevent.body} back from test service.")
+
+        return FINISHED
